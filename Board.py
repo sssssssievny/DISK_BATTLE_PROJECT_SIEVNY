efa20fbe6f992a316c3f54fc9ca47be35d8a0832
SIEVNY THAV <kneythav08@gmail.com>
2020-11-06 17:18:55 +0700

    Create game board

--- Board.py
+++ Board.py
@@ -1,1 +1,17 @@
-print('Hell World')
+theBoard = [
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]
+def showBoard(board):
+    for i in theBoard:
+        for j in i:
+            print(j,' ',end="")
+        print()
+
+showBoard(theBoard)
+
